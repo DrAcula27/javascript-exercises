fde9a03e6f57206e5af11630338022fdf18d1c02
Danielle <danielle@kendallspace.net>
2022-02-02 16:57:16 -0500

    fixed comments in repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -8,5 +8,47 @@ const repeatString = function(string, num) {
     return newString;
 };
 
+/*Methods to repeat a string in JavaScript
+
+1. While Loop:
+
+    //create empty string to hold new string
+    let repeatedString = "";
+
+    //create while loop
+    while (num > 0) {
+        repeatedString += string;
+        num--;
+    }
+
+    //return the result
+    return repeatedString
+
+2. Recursion
+
+    //define the terminal condition to end the recursion
+    if (num < 0) return "";
+
+    //special case for num === 1
+    if (num === 1) return string;
+
+    //recursion
+    else {
+        return string + repeatString(string, num - 1);
+    }
+
+3. ES6 repeat() method
+
+    //for non-negative num
+    if (num >= 0) {
+        return string.repeat(num);
+    } else { //if num is negative
+        return "ERROR";
+    }
+
+    //again, using the ternary operator - 
+    num >= 0 ? string.repeat(num) : "ERROR";
+*/
+
 // Do not edit below this line
 module.exports = repeatString;
