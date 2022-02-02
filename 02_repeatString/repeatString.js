37530b1d073c1ab00765d894adafebb196293f97
Danielle <danielle@kendallspace.net>
2022-02-02 16:09:23 -0500

    completed 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR"; //to account for negative numbers
 
+    let newString = ""; //to hold contents of string to return
+    for (let i = 0; i < num; i++) {
+        newString += string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
