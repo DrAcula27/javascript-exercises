b7615cf5e8b881910379c1d395b9b04f778c8189
Danielle <danielle@kendallspace.net>
2022-02-03 13:57:50 -0500

    removed .skip from spec file; started solution for tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  //formula: C = (F - 32) * (5/9)
+  //round result to 1 decimal place
+  return celsius = Math.round((((fTemp - 32) * (5/9)) * 10) / 10);
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  //formula: F = (C * (9/5)) + 32
+  //round result to 1 decimal place
+  return fahrenheit = Math.round((((cTemp * (9/5)) + 32) * 10) / 10);
 };
 
 // Do not edit below this line
