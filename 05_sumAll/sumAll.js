87ff451ff811a93f29baa950da326fbf2855cb67
Danielle <danielle@kendallspace.net>
2022-02-03 12:02:25 -0500

    removed '.skip' from all tests in sumAll.spec.js; completed 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    //check no non-number parameters
+    if ((typeof int1 !== "number") || typeof int2 !== "number") {
+        return "ERROR";
+    }
+    //check no negative parameters
+    if (int1 < 0 || int2 < 0) {
+        return "ERROR";
+    }
+    //sum parameters and all integers in between
+    let finalSum = 0;
+    if (int1 < int2) {
+        for (let i = int1; i <= int2; i++) {
+            finalSum += i;
+        }
+        return finalSum;
+    }
+    if (int1 > int2) {
+        for (let j = int2; j <= int1; j++) {
+            finalSum += j;
+        }
+        return finalSum;
+    }
 };
 
 // Do not edit below this line
