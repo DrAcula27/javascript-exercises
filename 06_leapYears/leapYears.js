668d0d441cfe1715ac97ae547c4d98c243771449
Danielle <danielle@kendallspace.net>
2022-02-03 13:41:59 -0500

    removed '.skip' from all tests in leapYears.spec.js; completed 06_leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //year is leap year if:
+        //The year is a multiple of 400. OR
+        //The year is a multiple of 4 and not a multiple of 100.
+    
+    if (((year % 400) === 0) || (((year % 4) === 0) && ((year % 100) !== 0))) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
