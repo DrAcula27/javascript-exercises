928f6b80e3920771b4f49ffe69b5ec8d444f3fb0
Danielle <danielle@kendallspace.net>
2022-02-03 11:30:44 -0500

    removed '.skip' from all tests in removeFromArray.spec.js; completed 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    array = array.filter(item => args.indexOf(item) === -1);
+    return array;
 };
+
+/*
+Another method: using includes() -> only works on latests browsers
+    array = array.filter(item => !args.includes(item));
+    return array;
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
