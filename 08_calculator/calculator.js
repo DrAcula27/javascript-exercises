526e6d36e8dd0aae089b20ae02273b8082835a94
Danielle <danielle@kendallspace.net>
2022-02-16 16:38:38 -0500

    removed .skip from tests; completed exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	return numArray.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+	return numArray.reduce((previousValue, currentValue) => previousValue * currentValue);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  return num === 0 ? 1 : (num * factorial(num - 1));
 };
 
 // Do not edit below this line
