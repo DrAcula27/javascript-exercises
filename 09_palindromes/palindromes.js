a4196d06a89946299507d3ee1f9963f19b500955
Danielle <danielle@kendallspace.net>
2022-02-16 16:52:39 -0500

    removed .skip from tests; completed exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    //find unwanted characters in the string
+    let regEx = /[\W_]/g;
 
+    //remove unwanted characters and make lowercase
+    let lowercaseEditStr = str.toLowerCase().replace(regEx, "");
+
+    //create a reversed str
+    reversedStr = lowercaseEditStr.split("").reverse().join("");
+
+    //return the comparison of the two strings
+    return lowercaseEditStr === reversedStr;
 };
 
 // Do not edit below this line
