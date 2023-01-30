1252a8e14128517d5f51c49fb4497c4648f1c008
Danielle <danielle@kendallspace.net>
2023-01-30 15:06:48 -0500

    complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,22 @@
-const convertToCelsius = function() {
+const preciseRound = function (value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function (temp) {
+  // formula: x°F = (x − 32) × 5/9 °C
+  let celsius = (temp - 32) * 5/9;
+  celsius = preciseRound(celsius, 1);
+  return celsius;
 };
+
+const convertToFahrenheit = function (temp) {
+  // formula: x°C = (x × 9/5 + 32) °F
+  let fahrenheit = (temp * (9/5) + 32);
+  fahrenheit = preciseRound(fahrenheit, 1);
+  return fahrenheit;
+};
+
 /*
 To accurately round to any number of decimal places, use exponential notation.
 See this article for more info:
@@ -11,5 +25,5 @@ https://medium.com/swlh/how-to-round-to-a-certain-number-of-decimal-places-in-ja
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
