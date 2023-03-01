3dd0ac8b82b6d42665a2b343c7639e5776d3f4b0
Danielle <danielle@kendallspace.net>
2023-03-01 12:02:22 -0500

    clean code and comments

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,15 +1,14 @@
-const fibonacci = function(num) {
-    //does not accept negatives
-    if(num < 0) {
-        return "OOPS";
-    }
-    //use Binet's formula to calculate Fibonacci of num
-    // (1/sqrt(5)) * (((1 + sqrt(5)) / 2)**n - ((1 - sqrt(5)) / 2)**n)
-    let squareRootOf5 = Math.sqrt(5);
-    let multiplier = 1 / squareRootOf5;
-    let posQuadratic = (1 + squareRootOf5) / 2;
-    let negQuadratic = (1 - squareRootOf5) / 2;
-    return Math.round(multiplier * (posQuadratic**num - negQuadratic**num));
+const fibonacci = function (num) {
+  // do not accept negatives
+  if (num < 0) return "OOPS";
+
+  // Binet's formula:
+  // (1/sqrt(5)) * (((1 + sqrt(5)) / 2)**n - ((1 - sqrt(5)) / 2)**n)
+  let squareRootOf5 = Math.sqrt(5);
+  let multiplier = 1 / squareRootOf5;
+  let posQuadratic = (1 + squareRootOf5) / 2;
+  let negQuadratic = (1 - squareRootOf5) / 2;
+  return Math.round(multiplier * (posQuadratic ** num - negQuadratic ** num));
 };
 
 // Do not edit below this line
